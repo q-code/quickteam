e8e6216a1bd25f73aa0bf771e12c75a0be66819c
q-code <phvandenberghe@gmail.com>
2014-06-15 09:20:30 +0200

    clean modules

--- bin/config.php
+++ bin/config.php
@@ -1,11 +1,10 @@
 <?php
-error_reporting(E_ALL);
-$qte_dbsystem = "mysql";
-$qte_host = "localhost";
-$qte_database = "qtcute";
-$qte_prefix = "";
-$qte_user = "qtcut_dbo";
-$qte_pwd = "dbo";
-$qte_port = "";
-$qte_dsn = "";
-$qte_install = "2008-05-18";
+$qte_dbsystem = 'mysql';
+$qte_host = 'localhost';
+$qte_database = 'quickteam';
+$qte_prefix = '';
+$qte_user = 'root';
+$qte_pwd = '';
+$qte_port = '';
+$qte_dsn = '';
+$qte_install = '';
--- language/nederlands/qtem_export.php
+++
@@ -1,5 +0,0 @@
-<?php
-$L['export']['Admin']='Export module';
-$L['export']['Content']='Content';
-$L['export']['Version']='Module versie';
-$L['export']['Filename']='Filename';
